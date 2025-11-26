509700bd781d2f52cefdeda952cf1f80d35bfbc9
Dominik Ilnicki <dominik.ilnicki@mechanical-orchard.com>
2025-11-26 07:44:31 +0100

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Ilnicki010 Homebrew Tap
+
+Homebrew formulas for my tools.
+
+## Installation
+
+```bash
+brew tap Ilnicki010/tap
+```
+
+## Available Tools
+
+- `prrompt` - Git post-commit hook for extracting prompts
---
+++ prrompt.rb
@@ -0,0 +1,5 @@
+class Prrompt < Formula
+  desc "Lightweight git post-commit hook that extracts prompts from commits"
+  homepage "https://github.com/Ilnicki010/prrompt"
+  # GoReleaser will populate url, version, and sha256 automatically
+end
